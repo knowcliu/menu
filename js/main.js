e90354e19c10b0671f579fdcac455dfd20b9898d
John J. Workman <johnjworkman@gmail.com>
2015-09-01 14:15:12 -0400

    Wire up basic video element

--- js/main.js
+++ js/main.js
@@ -17,8 +17,30 @@ function main()
 
     console.log('user media support ok!');
 
-    var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+
+    navigator.getUserMedia({video: true}, function(stream) {
+        var video = document.getElementById('video-id');
+        var canvas = document.getElementById('canvas-id');
+        var button = document.getElementById('button-id');
+
+        var url = window.url || URL;
+        video.src = url.createObjectURL(stream);
+        video.play();
+
+        button.disabled = false;
+
+        /*
+        button.onclick = function() {
+            canvas.getContext('2d')
+                .drawImage(video, 0, 0, 300, 300, 0, 0, 300, 300);
+        };
+        */
+    }, function(err) {
+        alert('there was an error');
+    });
 }
 
 main();
