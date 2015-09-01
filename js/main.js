86fbfc5fc5fa1066f983e0b9678be19aacebebf6
John J. Workman <johnjworkman@gmail.com>
2015-09-01 12:09:43 -0400

    Quick cleanups to JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-function hasGetUserMedia() {
+function hasGetUserMedia()
+{
+    'use strict';
+
     return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
         navigator.mozGetUserMedia || navigator.msGetUserMedia);
 }
 
-console.log(hasGetUserMedia() ? 'yay support!' : 'no user media support');
+function main()
+{
+    'use strict';
+
+    if (!hasGetUserMedia()) {
+        alert('no user media support');
+        return;
+    }
+
+    console.log('user media support ok!');
+
+    var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+}
+
+main();
